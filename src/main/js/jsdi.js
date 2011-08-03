44c7f6002bea051e3cd8a792dfa3e3dd290fac9d
unknown <jjomal@.na.ds.monsanto.com>
2011-08-03 10:33:37 -0500

    Handle a bean in the scope with a null value.

--- src/main/js/jsdi.js
+++ src/main/js/jsdi.js
@@ -21,141 +21,141 @@
  * Dependency injection for JavaScript that leverages the Java 5 approach.
  */
 var jsdi = {
-	/**
-	 * Internal place holder for a bean. Placeholders have a special
-	 * __beanReference__ property that gives them away.
-	 */
-	_BeanReference : function(id) {
-		this.id = id;
-		this.__jsdiBeanReference__ = true;
-	},
+    /**
+     * Internal place holder for a bean. Placeholders have a special
+     * __beanReference__ property that gives them away.
+     */
+    _BeanReference : function(id) {
+        this.id = id;
+        this.__jsdiBeanReference__ = true;
+    },
 
-	/**
-	 * @class a configuration responsible for wiring up beans. Basic Usage:
-	 *        <code>
-	 * var configuration = new jsdi.Configuration({
-	 *   beans : [ {
-	 *     type : A,
-	 *     ctor : function() {
-	 *       return new A();
-	 *     }
-	 *   }, {
-	 *     id : &quot;beanId&quot;,
-	 *     ctor : function() {
-	 *       return new B();
-	 *     }
-	 *   } ]
-	 * });
-	 * 
-	 * </code>
-	 * @param configuration
-	 */
-	Configuration : function(config) {
-		var self = this;
-		self.beans = config.beans;
-	},
+    /**
+     * @class a configuration responsible for wiring up beans. Basic Usage:
+     *        <code>
+     * var configuration = new jsdi.Configuration({
+     *   beans : [ {
+     *     type : A,
+     *     ctor : function() {
+     *       return new A();
+     *     }
+     *   }, {
+     *     id : &quot;beanId&quot;,
+     *     ctor : function() {
+     *       return new B();
+     *     }
+     *   } ]
+     * });
+     *
+     * </code>
+     * @param config the config object containing bean definitions.
+     */
+    Configuration : function(config) {
+        var self = this;
+        self.beans = config.beans;
+    },
 
-	/**
-	 * Inject a bean given a type. This is preferred to resource() as it makes
-	 * your code more portable across different application contexts.
-	 */
-	inject : function(type) {
-		if (typeof type === "function") {
-			return new jsdi._BeanReference(type);
-		} else {
-			throw new Error("Required type for inject(), got " + type);
-		}
-	},
+    /**
+     * Inject a bean given a type. This is preferred to resource() as it makes
+     * your code more portable across different application contexts.
+     */
+    inject : function(type) {
+        if (typeof type === "function") {
+            return new jsdi._BeanReference(type);
+        } else {
+            throw new Error("Required type for inject(), got " + type);
+        }
+    },
 
-	/**
-	 * Inject a bean given its identifier.
-	 */
-	resource : function(beanId) {
-		if (typeof beanId === "string") {
-			return new jsdi._BeanReference(beanId);
-		} else {
-			throw new Error("Required bean ID for resource(), got " + beanId);
-		}
-	}
+    /**
+     * Inject a bean given its identifier.
+     */
+    resource : function(beanId) {
+        if (typeof beanId === "string") {
+            return new jsdi._BeanReference(beanId);
+        } else {
+            throw new Error("Required bean ID for resource(), got " + beanId);
+        }
+    }
 
 };
 
 /**
  * Returns a bean for the given id, which should either be a String identifier
  * or a type (function).
- * 
+ *
  * @param id
  *            the bean Id.
  */
 jsdi.Configuration.prototype.getBean = function(id) {
-	var self = this;
-	return self._resolveBean(self._beanDef(id));
+    var self = this;
+    return self._resolveBean(self._beanDef(id));
 };
 
 /**
  * Resolves all beans given a scope.
- * 
+ *
  * @param scope
  *            the object to search bean definitions for.
  */
 jsdi.Configuration.prototype.resolve = function(scope) {
-	var self = this, i, obj;
-	for (i in scope) {
-		obj = scope[i];
-		if (obj.__jsdiBeanReference__) {
-			scope[i] = self._resolveBean(self._beanDef(obj.id));
-		}
-	}
+    var self = this, i, obj;
+    for (i in scope) {
+        obj = scope[i];
+        if (obj && obj.__jsdiBeanReference__) {
+            scope[i] = self._resolveBean(self._beanDef(obj.id));
+        }
+    }
 };
 
 /**
  * Return a bean definition given either a type or name.
  */
 jsdi.Configuration.prototype._beanDef = function(id) {
-	var self = this, i, beanDefinition, result, description;
+    var self = this, i, beanDefinition, result, description;
 
-	for (i in self.beans) {
-		beanDefinition = self.beans[i];
-		if (beanDefinition.id === id || beanDefinition.type === id) {
-			result = beanDefinition;
-			break;
-		}
-	}
-	if (!result) {
-		if (id.constructor && id.constructor.name) {
-			description = id.constructor.name;
-		} else {
-			description = id;
-		}
-		throw "No bean definition found for " + description;
-	}
-	return result;
+    for (i in self.beans) {
+        beanDefinition = self.beans[i];
+        if (beanDefinition.id === id || beanDefinition.type === id) {
+            result = beanDefinition;
+            break;
+        }
+    }
+    if (!result) {
+        if (id.constructor && id.constructor.name) {
+            description = id.constructor.name;
+        } else {
+            description = id;
+        }
+        throw "No bean definition found for " + description;
+    }
+    return result;
 };
 
 /**
  * Resolve a bean definition to an actual bean.
  */
 jsdi.Configuration.prototype._resolveBean = function(beanDefinition) {
-	var self = this, bean, property;
-	
-	if (!beanDefinition.bean || beanDefinition.scope === 'prototype') {
-		bean = beanDefinition.ctor();
-		beanDefinition.bean = bean;
-		for (property in bean) {
-			self._resolveBeanProperty(bean, property);
-		}
-	}
-	return beanDefinition.bean;
+    var self = this, bean, property;
+
+    if (!beanDefinition.bean || beanDefinition.scope === 'prototype') {
+        bean = beanDefinition.ctor();
+        beanDefinition.bean = bean;
+        for (property in bean) {
+            self._resolveBeanProperty(bean, property);
+        }
+    }
+    return beanDefinition.bean;
 };
 
 /**
- * Copy a bean's properties.
+ * Resolve any unresolved references in the given bean.
  */
 jsdi.Configuration.prototype._resolveBeanProperty = function(bean, property) {
-	var self = this, value;
-	
-	value = bean[property];
-	if (value instanceof jsdi._BeanReference) {
-		bean[property] = self.getBean(value.id);
-	}
+    var self = this, value;
+
+    value = bean[property];
+    if (value instanceof jsdi._BeanReference) {
+        bean[property] = self.getBean(value.id);
+    }
 };
--- src/test/js/jsdiTest.js
+++ src/test/js/jsdiTest.js
@@ -25,130 +25,168 @@ var raises = QUnit.raises;
 var test = QUnit.test;
 
 function B() {
-	this.name = "B";
+    this.name = "B";
 }
 
 function A() {
-	var self = this;
-	self.valueSetInConstructor = 42;
-	self.b = jsdi.inject(B);
+    var self = this;
+    //noinspection JSUnusedGlobalSymbols
+    self.valueSetInConstructor = 42;
+    self.b = jsdi.inject(B);
 }
 
 test("get bean by type", function() {
-	var a, configuration;
+    var a, configuration;
 
-	configuration = new jsdi.Configuration({
-		beans : [ {
-			type : A,
-			ctor : function() {
-				return new A();
-			}
-		}, {
-			type : B,
-			ctor : function() {
-				return new B();
-			}
-		} ]
-	});
+    configuration = new jsdi.Configuration({
+        beans : [
+            {
+                type : A,
+                ctor : function() {
+                    return new A();
+                }
+            },
+            {
+                type : B,
+                ctor : function() {
+                    return new B();
+                }
+            }
+        ]
+    });
 
-	a = configuration.getBean(A);
+    a = configuration.getBean(A);
 
-	ok(a.b.name === "B");
+    ok(a.b.name === "B");
 });
 
 test("get bean by id", function() {
-	var a, configuration, x, y;
+    var configuration, x, y;
 
-	configuration = new jsdi.Configuration({
-		beans : [ {
-			id : "x",
-			ctor : function() {
-				return {
-					id : "x"
-				};
-			}
-		}, {
-			id : "y",
-			ctor : function() {
-				return {
-					x : jsdi.resource("x")
-				};
-			}
-		} ]
-	});
+    configuration = new jsdi.Configuration({
+        beans : [
+            {
+                id : "x",
+                ctor : function() {
+                    return {
+                        id : "x"
+                    };
+                }
+            },
+            {
+                id : "y",
+                ctor : function() {
+                    return {
+                        x : jsdi.resource("x")
+                    };
+                }
+            }
+        ]
+    });
 
-	test("prototype scope", function() {
-		var configuration;
+    y = configuration.getBean("y");
 
-		configuration = new jsdi.Configuration({
-			beans : [ {
-				type : B,
-				scope : "prototype",
-				ctor : function() {
-					return new B();
-				}
-			}, {
-				id : "x",
-				ctor : function() {
-					var self = {};
-					self.b = jsdi.inject(B);
-					return self;
-				}
-			}, {
-				type : A,
-				ctor : function() {
-					return new A();
-				}
-			} ]
-		});
-		x = configuration.getBean("x");
-		a = configuration.getBean(A);
-		ok(a.b);
-		ok(x.b);
-		ok(a.b !== x.b);
-	});
+    equals("x", y.x.id);
+});
 
-	y = configuration.getBean("y");
+test("prototype scope", function() {
+    var configuration, a, x;
 
-	equals("x", y.x.id);
+    configuration = new jsdi.Configuration({
+        beans : [
+            {
+                type : B,
+                scope : "prototype",
+                ctor : function() {
+                    return new B();
+                }
+            },
+            {
+                id : "x",
+                ctor : function() {
+                    var self = {};
+                    self.b = jsdi.inject(B);
+                    return self;
+                }
+            },
+            {
+                type : A,
+                ctor : function() {
+                    return new A();
+                }
+            }
+        ]
+    });
+    x = configuration.getBean("x");
+    a = configuration.getBean(A);
+    ok(a.b);
+    ok(x.b);
+    ok(a.b !== x.b);
 });
 
 test("inject requires function", function() {
-	raises(function() {
-		jsdi.inject(42);
-	});
+    raises(function() {
+        jsdi.inject(42);
+    });
 });
 
 test("requires string id", function() {
-	raises(function() {
-		jsdi.resource(42);
-	});
+    raises(function() {
+        jsdi.resource(42);
+    });
 });
 
 test("resolve", function() {
-	var a, c;
+    var a, c;
 
-	a = {
-		b : jsdi.inject(B),
-		d : {
-			id : "rubbish",
-			value : "some value"
-		}
-	};
+    a = {
+        b : jsdi.inject(B),
+        d : {
+            id : "rubbish",
+            value : "some value"
+        }
+    };
 
-	c = new jsdi.Configuration({
-		beans : [ {
-			type : B,
-			ctor : function() {
-				return new B();
-			}
-		} ]
-	});
+    c = new jsdi.Configuration({
+        beans : [
+            {
+                type : B,
+                ctor : function() {
+                    return new B();
+                }
+            }
+        ]
+    });
 
-	equals(a.b.name, undefined);
+    equals(a.b.name, undefined);
 
-	c.resolve(a);
+    c.resolve(a);
 
-	equals(a.b.name, "B");
-	equals(a.d.value, "some value");
+    equals(a.b.name, "B");
+    equals(a.d.value, "some value");
+});
+
+test("resolve accepts scope with null value", function() {
+    var scope, configuration;
+
+    scope = {
+        nullBean: null,
+        notNullBean: jsdi.inject(B)
+    };
+
+    configuration = new jsdi.Configuration({
+        beans: [
+            {
+                type: B,
+                ctor: function() {
+                    return new B();
+                }
+            }
+        ]
+    });
+
+    configuration.resolve(scope);
+
+    ok(scope.nullBean === null);
+    ok(scope.notNullBean instanceof B);
 });
